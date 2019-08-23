86dcf9603cbd83d0a0e59534c6a1a3aef142020e
DrSavauG <46158444+DrSavauG@users.noreply.github.com>
2019-08-23 15:56:51 +0300

    Update PureFunction.js

--- HomeWorks/A2+/PureFunction.js
+++ HomeWorks/A2+/PureFunction.js
@@ -1,16 +1,10 @@
-let string = "  z zz3z333z3z                                                     q                                              d                                                              ";
-
 function deleteFirstLastGap(input) {
     var newString = JSON.parse(JSON.stringify(input));
-    
     while (newString.charAt(0) == ' ') {
         newString = newString.substr(1);
-        }
+    }
     while (newString.charAt(newString.length - 1) == ' ') {
-        newString = newString.substr(0,newString.length - 1);
-        }
-
+        newString = newString.substr(0, newString.length - 1);
+    }
     return newString;
 }
-console.log(deleteFirstLastGap(string));
-
