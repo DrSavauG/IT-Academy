295ca7683b3c6421be2d458cc4ef92d2709473da
DrSavauG <46158444+DrSavauG@users.noreply.github.com>
2019-08-24 17:02:11 +0300

    Update PureFunction.js
    заменил на substring

--- HomeWorks/A2+/PureFunction.js
+++ HomeWorks/A2+/PureFunction.js
@@ -1,11 +1,38 @@
-function deleteFirstLastGap(newString) {
-
+function deleteFirstLastGap(string) {
     var from = 0;
-    while (newString.charAt(from) == ' ') from++;
-    var to = newString.length - 1;
-    while (newString.charAt(to) == ' ') to--;
-    newString = newString.substr(from, to + 1);
-    return newString;
+    var to = string.length - 1;
+    if (string.charAt(from) != ' ' && string.charAt(to) != ' ') {
+        return string;
+    } else {
+        for (from; string.charAt(from) == ' '; from++);
+        if (from == string.length) {
+            return ''; // или undefined?
+        } else {
+            for (to; string.charAt(to) == ' '; to--);
+            string = string.substring(from, to + 1);//.substr некорректно убирал справа
+            return string;
+        }
+    }
 }
-let input = prompt(`Введите строку... `,'');
+let input = prompt(`Введите строку... `, '');
 console.log(deleteFirstLastGap(input));
+
+/*
+// для тестирования
+let input2 = ' ';
+console.log(deleteFirstLastGap(input2));
+input2 = '           ';
+console.log(deleteFirstLastGap(input2));
+input2 = '   <пробел>           ';
+console.log(deleteFirstLastGap(input2));
+input2 = '               <пробел>   ';
+console.log(deleteFirstLastGap(input2));
+input2 = '          <пробел>  ';
+console.log(deleteFirstLastGap(input2));
+input2 = ' <пробел>                   ';
+console.log(deleteFirstLastGap(input2));
+input2 = '                <пробел!';
+console.log(deleteFirstLastGap(input2));
+input2 = '!пробел>                   ';
+console.log(deleteFirstLastGap(input2));
+*/
