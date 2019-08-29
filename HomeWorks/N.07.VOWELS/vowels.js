43c34561e45341a3f5294c2ee6e1afeb6df77b6a
DrSavauG <46158444+DrSavauG@users.noreply.github.com>
2019-08-29 12:30:39 +0300

    Update vowels.js

--- HomeWorks/N.07.VOWELS/vowels.js
+++ HomeWorks/N.07.VOWELS/vowels.js
@@ -1,1 +1,39 @@
 "use strict";
+function vowels(input) {
+    const stringLowerSplit = input.toLowerCase().split('');;
+    var sum = 0;
+    stringLowerSplit.forEach(element => {
+        if (element == 'а' ||
+            element == 'о' ||
+            element == 'и' ||
+            element == 'е' ||
+            element == 'ё' ||
+            element == 'э' ||
+            element == 'ы' ||
+            element == 'у' ||
+            element == 'ю' ||
+            element == 'я') {
+            sum++;
+        }
+
+    });
+    return sum;
+}
+function vowels2(input) {
+    const stringLowerSplit = input.toLowerCase().split('');;
+    return stringLowerSplit.filter(element =>
+        element == 'а' ||
+        element == 'о' ||
+        element == 'и' ||
+        element == 'е' ||
+        element == 'ё' ||
+        element == 'э' ||
+        element == 'ы' ||
+        element == 'у' ||
+        element == 'ю' ||
+        element == 'я').length
+}
+let userString = prompt(`Введите строку...`, 'аоиеёэыуюяАОИЕЁЭЫУЮЯ');
+console.log(vowels2(userString));
+console.log(vowels(userString));
+
