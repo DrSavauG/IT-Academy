f256b480d70419d3f0608423ace04a931abb9b2d
DrSavauG <46158444+DrSavauG@users.noreply.github.com>
2019-08-29 12:38:47 +0300

    оставил  2й вариант

--- HomeWorks/N.07.VOWELS/vowels.js
+++ HomeWorks/N.07.VOWELS/vowels.js
@@ -1,26 +1,6 @@
 "use strict";
 function vowels(input) {
     const stringLowerSplit = input.toLowerCase().split('');;
-    var sum = 0;
-    stringLowerSplit.forEach(element => {
-        if (element == 'а' ||
-            element == 'о' ||
-            element == 'и' ||
-            element == 'е' ||
-            element == 'ё' ||
-            element == 'э' ||
-            element == 'ы' ||
-            element == 'у' ||
-            element == 'ю' ||
-            element == 'я') {
-            sum++;
-        }
-
-    });
-    return sum;
-}
-function vowels2(input) {
-    const stringLowerSplit = input.toLowerCase().split('');;
     return stringLowerSplit.filter(element =>
         element == 'а' ||
         element == 'о' ||
@@ -34,6 +14,6 @@ function vowels2(input) {
         element == 'я').length
 }
 let userString = prompt(`Введите строку...`, 'аоиеёэыуюяАОИЕЁЭЫУЮЯ');
-console.log(vowels2(userString));
 console.log(vowels(userString));
+
 
