039cfb5a7f53c793fa7c9bf9c7d2412f853647cd
DrSavauG <46158444+DrSavauG@users.noreply.github.com>
2019-08-31 21:41:52 +0300

    тройная
    добавил три вила foreach, reduce, filtr

--- HomeWorks/N.07.VOWELS/vowels.js
+++ HomeWorks/N.07.VOWELS/vowels.js
@@ -1,8 +1,32 @@
 "use strict";
-function vowels(input) {
-    let vowelsArr = ['а', 'о', 'и', 'е', 'ё', 'э', 'ы', 'у', 'ю', 'я'];
+function vowelsFilter(input) {
+    const vowelsArr = ['а', 'о', 'и', 'е', 'ё', 'э', 'ы', 'у', 'ю', 'я'];
     return input.toLowerCase().split('').filter(element => {
-        return vowelsArr.indexOf(element) !== -1;}).length;
-    }
+        return vowelsArr.indexOf(element) !== -1;
+    }).length;
+}
+
+function vowelsForEach(input) {
+    const vowelsArr = ['а', 'о', 'и', 'е', 'ё', 'э', 'ы', 'у', 'ю', 'я'];
+    let sum = 0;
+    input.toLowerCase().split('').forEach(element => {
+        if (vowelsArr.indexOf(element) !== -1) sum++
+    });
+    return sum;
+}
+
+function vowelsReduce(input) {
+    const vowelsArr = ['а', 'о', 'и', 'е', 'ё', 'э', 'ы', 'у', 'ю', 'я'];
+    return input.toLowerCase().split('').reduce((sum, element) => {
+        if (vowelsArr.indexOf(element) !== -1) {
+            return sum + 1;
+        } else {
+            return sum + 0;
+        }
+    }, 0);
+}
+
 let userString = prompt(`Введите строку...`, 'аоиеёэыуюяАОИЕЁЭЫУЮЯ');
-    console.log(vowels(userString));
+console.log(vowelsFilter(userString));
+console.log(vowelsForEach(userString));
+console.log(vowelsReduce(userString));
