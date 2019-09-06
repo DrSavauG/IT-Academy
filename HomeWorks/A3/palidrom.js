09ec9039715290020c4b359f774252906465ba15
DrSavauG <46158444+DrSavauG@users.noreply.github.com>
2019-09-06 14:27:03 +0300

    add for

--- HomeWorks/A3/palidrom.js
+++ HomeWorks/A3/palidrom.js
@@ -1,8 +1,19 @@
 'use strict';
-let phrase = prompt(`Введите фразу...`, '');
-function isPalindrom(input) {
+
+function isPalindrom(input) {// с неявным массивом
     let fixPhrase = input.replace(/[ёЁ]/g, 'е').toLowerCase().replace(/[^а-щыэ-я]/g, '')
     let fixPhraseReverse = fixPhrase.split('').reverse().join('');
     return fixPhrase === fixPhraseReverse;
 }
+
+function isPalindromFor(input) {// через for
+    let fixPhrase = input.replace(/[ёЁ]/g, 'е').toLowerCase().replace(/[^а-щыэ-я]/g, '')
+    for (let index = 0; index < Math.floor(fixPhrase.length / 2); index++) {
+        if (fixPhrase[index] !== fixPhrase[fixPhrase.length - index - 1]) {
+            return false
+        }
+    } return true
+}
+let phrase = prompt(`Введите фразу...`, '');
 console.log(isPalindrom(phrase));
+console.log(isPalindromFor(phrase));
