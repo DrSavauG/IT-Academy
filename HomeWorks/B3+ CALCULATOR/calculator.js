0b261982c4663bbac4e7e63afb30bcec70420638
DrSavauG <46158444+DrSavauG@users.noreply.github.com>
2019-09-10 16:22:45 +0300

    допилил, работаем но без скобок

--- HomeWorks/B3+ CALCULATOR/calculator.js
+++ HomeWorks/B3+ CALCULATOR/calculator.js
@@ -3,21 +3,34 @@
 // Должны работать операции + - * / и скобки, числа должны приниматься целые, дробные (через точку), отрицательные
 // Например, вызываем функцию, передавая ей строку "2*(-3+1)", функция возвращает -4.
 'use string';
+function calculate(params) {
+    let sum = 0;
+    // if (params.indexOf("(")) {
+    //     let x = params.indexOf("(");
+    //     let y = params.lastIndexOf(")");
+    //     calculate(params.substring(x, y + 1).slice(1, -1));
+    // }
+    let enteredCut = params.replace(/\s/g, '').replace(/[-]/g, '•-').replace(/[+]/g, '•+').replace(/[*]/g, '•*•').replace(/[/]/g, '•/•').split('•').filter(nul => nul);
 
-let entered = "-1+5/-2*4/-3";
-enteredCut = entered.replace(/\s/g, '').replace(/[-]/g, '•-').replace(/[+]/g, '•+•').replace(/[*]/g, '•*•').replace(/[/]/g, '•/•').split('•').filter(nul => nul);
-// 
-console.log(enteredCut);
-// 
-function counting(mathSigh) {
-    while (enteredCut.includes(mathSigh)) {
-        let i = enteredCut.indexOf(mathSigh);
-        enteredCut.splice(enteredCut.indexOf(mathSigh) - 1, 3, enteredCut[i - 1] / enteredCut[i + 1])
-    }    
+    while (enteredCut.includes('/')) {
+        let i = enteredCut.indexOf('/');
+        enteredCut.splice(enteredCut.indexOf('/') - 1, 3, +enteredCut[i - 1] / +enteredCut[i + 1]);
+    }
+
+    while (enteredCut.includes('*')) {
+        let i = enteredCut.indexOf('*');
+        enteredCut.splice(enteredCut.indexOf('*') - 1, 3, +enteredCut[i - 1] * +enteredCut[i + 1]);
+    }
+
+    enteredCut = enteredCut.join('•+•').split('•');
+
+    while (enteredCut.includes('+')) {
+        let i = enteredCut.indexOf('+');
+        enteredCut.splice(enteredCut.indexOf('+') - 1, 3, +enteredCut[i - 1] + +enteredCut[i + 1]);
+    }
+    return sum + Number(enteredCut.join());
 }
-counting('/');
-console.log(enteredCut);
-counting('*');
-console.log(enteredCut);
-counting('+');
-console.log(enteredCut);
+let str = "2*(-3+1)";
+console.log(calculate(str));
+
+console.log(eval(str));//проверка eval-om
