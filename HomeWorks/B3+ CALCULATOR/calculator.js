ee171406cd46fae1f5e9b716bd9d0c64ecb8c0cd
DrSavauG <46158444+DrSavauG@users.noreply.github.com>
2019-09-12 14:46:12 +0300

    (1)-(1))
    теперь ((1)-(1)) катит

--- HomeWorks/B3+ CALCULATOR/calculator.js
+++ HomeWorks/B3+ CALCULATOR/calculator.js
@@ -5,13 +5,19 @@
 'use string';
 function calculate(params) {
     let arr = params.replace(/\s/g, '').replace(/[-]/g, '•-•').replace(/[+]/g, '•+•').replace(/[*]/g, '•*•').replace(/[/]/g, '•/•').replace(/[(]/g, '•(•').replace(/[)]/g, '•)•').split('•').filter(nul => nul);
-
     function counting(arr) {
         while (arr.includes("(")) {
             let a = arr.indexOf("(");
             let a1 = arr.indexOf('(', a + 1);
+            let a2 = arr.indexOf('(', a1 + 1);
             let b = arr.indexOf(")");
             let b1 = arr.indexOf(")", b + 1);
+            let b2 = arr.indexOf(")", b1 + 1);
+            if (a2 < b1 && a2 !== -1) {
+                let insideHooks = arr.splice(a, b2 - a + 1).slice(1, -1);
+                arr.splice(a, 0, counting(insideHooks));
+                continue;
+            }
             if (a1 !== -1 && b > a1) {
                 let insideHooks = arr.splice(a, b1 - a + 1).slice(1, -1);
                 arr.splice(a, 0, counting(insideHooks));
@@ -47,5 +53,5 @@ function calculate(params) {
     return counting(arr);
 }
 let str = prompt('Введите выражение', "3+1/(-2-(9)-3*2)-2*(-(-9))");
-// let str = "3+1/(-2-(9)-3*2)-2*(-(-9))";
+// let str = "(((1)))+((1))";
 console.log(`${calculate(str)}  ||  ${eval(str) == calculate(str)}`);
--- test.js
+++ test.js
@@ -1,22 +1,2 @@
-let str = '1(23(4)5)6(7)89';
-let arr = str.split('');
-let zzz = 0;;
-let zzz1 = 1;
-
-// while (arr.includes("(")) {
-
-let a = arr.indexOf("(");
-let a1 = arr.indexOf('(', a + 1);
-let b = arr.indexOf(")");
-let b1 = arr.indexOf(')', b + 1)
-if (a1 !== -1 && b > a1) {
-    let insideHooks = arr.splice(a1, b - a1 + 1).slice(1, -1);
-    arr.splice(a1, 0, zzz);
-}
-if (a !== -1) {
-    let insideHooks = arr.splice(a, b - a + 1).slice(1, -1);
-    arr.splice(a, 0, zzz1);
-}
-// }
-
-console.log(arr);
+let str = '(2)(-1)';
+console.log(eval(str));
