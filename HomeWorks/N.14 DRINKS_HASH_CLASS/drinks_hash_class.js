dec46a3635d3bdf7400813bef4680a381676ab2a
DrSavauG <46158444+DrSavauG@users.noreply.github.com>
2019-09-18 23:26:38 +0300

    fix
    fix от this._func

--- HomeWorks/N.14 DRINKS_HASH_CLASS/drinks_hash_class.js
+++ HomeWorks/N.14 DRINKS_HASH_CLASS/drinks_hash_class.js
@@ -17,11 +17,9 @@ class HashStorageFunc {
     deleteValue(delKey) {
         if (delKey in this.storage) {
             delete this.storage[delKey];
-            console.warn(`Напиток ${delKey} удалён!`);
             return true;
         }
         else {
-            console.error(`Напиток ${delKey} не  был передан!`);
             return false;
         }
     };
@@ -30,6 +28,50 @@ class HashStorageFunc {
 // 2
 var drinkStorage = new HashStorageFunc();
 // 3
+addButton.onclick = () => {
+    do {
+        var drinkName = prompt('Введите название напитка', '');
+        if (drinkName === null) return false;
+        if (!drinkName) console.error(`Название напитка не может быть пустым!`);
+    } while (!drinkName);
+    var alco = confirm(`Алкогольный? `);
+    var recept = prompt(`Введите рецепт... `, '');
+    drinkStorage.addValue(drinkName, { a: alco, r: recept })
+}
+getButton.onclick = () => {
+    do {
+        var drinkName = prompt(`Введите название напитка ...`, '');
+        if (drinkName === null) return false;
+        if (!drinkName) console.error(`Название напитка не может быть пустым!`);
+    } while (!drinkName);
+    var i = drinkStorage.getValue(drinkName);
+    if (i) {
+        console.info(`Напиток: ${drinkName}\nАлкогольный: ${i.a ? 'Да' : 'Нет'}\nРецепт: ${i.r}`)
+    } else {
+        console.error(`Напиток "${drinkName}" ещё не  был внесен!`);
+    }
+}
+deleteButton.onclick = () => {
+    do {
+        var delKey = prompt('Введите название напитка для удаления', '');
+        if (delKey === null) return false;
+        if (!delKey) console.error(`Название удаляемого напитка не может быть пустым!`);
+    } while (!delKey);
+    if (drinkStorage.getValue(delKey)) {
+        drinkStorage.deleteValue(delKey);
+        console.warn(`Напиток "${delKey}" удалён!`);
+    } else {
+        console.error(`Напитка "${delKey}" нет в списке!`);
+    }
+}
+listButton.onclick = () => {
+    if (drinkStorage.getKeys() == false) {
+        console.error(`Список напитков пуст!`);
+    } else {
+        console.info(drinkStorage.getKeys().join('\n'));
+    }
+}
+/*
 addButton.onclick = () => {
     var drinkName = prompt('Введите название напитка', '');
     var alco = confirm(`Алкогольный? `);
@@ -50,3 +92,4 @@ deleteButton.onclick = () => {
 listButton.onclick = () => {
     console.info(drinkStorage.getKeys().join('\n'));
 }
+*/
