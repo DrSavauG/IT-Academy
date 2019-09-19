b8ff9811f3c0efbe7952c3b8a3719d4a178bcbee
DrSavauG <46158444+DrSavauG@users.noreply.github.com>
2019-09-19 10:03:52 +0300

    fix
    доправил,
    зацикливает на названии
    выводит список с названиями в алерте

--- HomeWorks/N.14 DRINKS_HASH_CLASS/drinks_hash_class.js
+++ HomeWorks/N.14 DRINKS_HASH_CLASS/drinks_hash_class.js
@@ -6,7 +6,6 @@ class HashStorageFunc {
     }
     addValue(key, value) {
         this.storage[key] = value;
-        console.log(this.storage);
     }
     getValue(key) {
         return this.storage[key];
@@ -22,74 +21,67 @@ class HashStorageFunc {
         else {
             return false;
         }
-    };
-
+    }
 }
 // 2
 var drinkStorage = new HashStorageFunc();
 // 3
-addButton.onclick = () => {
+function _sticking() {
     do {
-        var drinkName = prompt('Введите название напитка', '');
-        if (drinkName === null) return false;
-        if (!drinkName) console.error(`Название напитка не может быть пустым!`);
-    } while (!drinkName);
+        let i = drinkStorage.getKeys()
+        var input = prompt(`Введите название напитка...\n${i == false ? '' : `В наличии:\n   •${i.join('\n   •')}`}`, '');
+        if (input === null) {
+            console.warn(`Ввод был отменен`);
+            return false;
+        }
+        input = input.trim().toUpperCase();
+        if (!input) console.error(`Название напитка не может быть пустым!`);
+    } while (!input);
+    return input
+}
+addButton.onclick = () => {
+    var drinkName = _sticking()
+    if (drinkName === false) return false;
     var alco = confirm(`Алкогольный? `);
     var recept = prompt(`Введите рецепт... `, '');
     drinkStorage.addValue(drinkName, { a: alco, r: recept })
+    console.info(`Добавлен новый напиток "${drinkName}"`)
 }
 getButton.onclick = () => {
-    do {
-        var drinkName = prompt(`Введите название напитка ...`, '');
-        if (drinkName === null) return false;
-        if (!drinkName) console.error(`Название напитка не может быть пустым!`);
-    } while (!drinkName);
-    var i = drinkStorage.getValue(drinkName);
-    if (i) {
-        console.info(`Напиток: ${drinkName}\nАлкогольный: ${i.a ? 'Да' : 'Нет'}\nРецепт: ${i.r}`)
+    if (drinkStorage.getKeys() == false) {
+        console.error(`Список напитков пуст!`);
     } else {
-        console.error(`Напиток "${drinkName}" ещё не  был внесен!`);
+        do {
+            var drinkName = _sticking();
+            if (drinkName === false) return false;
+            var i = drinkStorage.getValue(drinkName);
+            console.error(`Напиток "${drinkName}" ещё не  был внесен!`);
+        } while (!i);
+        console.info(`Напиток: ${drinkName}\nАлкогольный: ${i.a ? 'Да' : 'Нет'}\nРецепт: ${i.r}`)
     }
 }
 deleteButton.onclick = () => {
-    do {
-        var delKey = prompt('Введите название напитка для удаления', '');
-        if (delKey === null) return false;
-        if (!delKey) console.error(`Название удаляемого напитка не может быть пустым!`);
-    } while (!delKey);
-    if (drinkStorage.getValue(delKey)) {
-        drinkStorage.deleteValue(delKey);
-        console.warn(`Напиток "${delKey}" удалён!`);
+    if (drinkStorage.getKeys() == false) {
+        console.error(`Список напитков пуст!`);
     } else {
-        console.error(`Напитка "${delKey}" нет в списке!`);
+        do {
+            var delKey = _sticking();
+            if (delKey === false) return false;
+            var i = drinkStorage.getValue(delKey);
+            if (!i) console.error(`Напиток "${delKey}" отсутствует в списке!`);
+        } while (!i);
+        if (i) {
+            drinkStorage.deleteValue(delKey);
+            console.warn(`Напиток "${delKey}" удалён!`);
+        } else {
+            console.error(`Напитка "${delKey}"ещё нет в списке!`);
+        }
     }
 }
 listButton.onclick = () => {
     if (drinkStorage.getKeys() == false) {
         console.error(`Список напитков пуст!`);
     } else {
-        console.info(drinkStorage.getKeys().join('\n'));
+        console.info(`Список напитков:\n${drinkStorage.getKeys().join('\n')}`);
     }
 }
-/*
-addButton.onclick = () => {
-    var drinkName = prompt('Введите название напитка', '');
-    var alco = confirm(`Алкогольный? `);
-    var recept = prompt(`Введите рецепт... `, '');
-    // return
-    drinkStorage.addValue(drinkName, { a: alco, r: recept })
-
-}
-getButton.onclick = () => {
-    var drinkName = prompt(`Введите название напитка ...`, '');
-    var i = drinkStorage.getValue(drinkName);
-    console.info(`Напиток: ${drinkName}\nАлкогольный: ${i.a ? 'Да' : 'Нет'}\nРецепт: ${i.r}`)
-}
-deleteButton.onclick = () => {
-    var delKey = prompt('Введите название напитка для удаления', '');
-    drinkStorage.deleteValue(delKey);
-}
-listButton.onclick = () => {
-    console.info(drinkStorage.getKeys().join('\n'));
-}
-*/
